5c826b461975f4bab321d945e2b4d993292fa0b5
Mike Keesey <keesey@gmail.com>
2022-04-12 11:06:02 -0700

    Added detection for arrays.

--- packages/phylopic-utils/src/models/detection/isObject.ts
+++ packages/phylopic-utils/src/models/detection/isObject.ts
@@ -1,5 +1,5 @@
 import { invalidate } from "../../validation"
 import { ValidationFaultCollector } from "../../validation/ValidationFaultCollector"
-const isObject = (x: unknown, faultCollector?: ValidationFaultCollector): x is NonNullable<object> =>
+export const isObject = (x: unknown, faultCollector?: ValidationFaultCollector): x is NonNullable<object> =>
     (typeof x === "object" && x !== null) || invalidate(faultCollector, "Expected an object.")
 export default isObject
