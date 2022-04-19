0122c0f54bcc477b8a84f01bb5edc5ce4ca526f6
Mike Keesey <keesey@gmail.com>
2022-04-19 13:24:58 -0700

    Added compareStrings utility function.

---
+++ packages/phylopic-utils/src/comparison/compareStrings.ts
@@ -0,0 +1,2 @@
+export const compareStrings = (a: string, b: string) => (a < b ? -1 : a > b ? 1 : 0)
+export default compareStrings
--- packages/phylopic-utils/src/index.ts
+++ packages/phylopic-utils/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./aws"
+export * from "./comparison"
 export * from "./detection"
 export * from "./http"
 export * from "./identification"
