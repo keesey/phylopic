d7c2bf5b6b3fbfc9d22d38a8bf786ad1102ab976
Mike Keesey <keesey@gmail.com>
2022-05-09 16:18:44 -0700

    Initial version of dropdown nav

--- packages/webapp/src/ui/SiteNav/index.tsx
+++ packages/webapp/src/ui/SiteNav/index.tsx
@@ -1,17 +1,33 @@
-import { FC } from "react"
+import { useRouter } from "next/router"
+import { FC, useCallback, useEffect, useState } from "react"
 import SearchBar from "../../search/SearchBar"
 import AnchorLink from "../AnchorLink"
 import SiteTitle from "../SiteTitle"
+import DropdownNav from "./DropdownNav"
 import styles from "./index.module.scss"
-const SiteNav: FC = () => (
-    <nav className={styles.main}>
-        <AnchorLink key="title" className={styles.siteTitle} href="/">
-            <SiteTitle />
-        </AnchorLink>
-        <SearchBar key="search" />
-        <div key="menuButton" className={styles.menuButton}>
-            <button>☰</button>
-        </div>
-    </nav>
-)
+const SiteNav: FC = () => {
+    const [dropdownOpen, setDropdownOpen] = useState(false)
+    const router = useRouter()
+    const handleMenuButtonClick = useCallback(() => setDropdownOpen(!dropdownOpen), [dropdownOpen])
+    const handleDropdownNavClose = useCallback(() => setDropdownOpen(false), [])
+    useEffect(() => {
+        const handler = () => setDropdownOpen(false)
+        router.events.on("routeChangeStart", handler)
+        return () => router.events.off("routeChangeStart", handler)
+    }, [router])
+    return (
+        <nav className={styles.main}>
+            <AnchorLink key="title" className={styles.siteTitle} href="/">
+                <SiteTitle />
+            </AnchorLink>
+            <SearchBar key="search" />
+            <div key="menuButton" className={styles.menuButton}>
+                <button onClick={handleMenuButtonClick}>☰</button>
+            </div>
+            {dropdownOpen && (
+                <DropdownNav onClose={handleDropdownNavClose} />
+            )}
+        </nav>
+    )
+}
 export default SiteNav
