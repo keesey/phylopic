c415697cb36a008b08306c1d59cb2a76eb457731
Mike Keesey <keesey@gmail.com>
2022-05-09 22:31:30 -0700

    File formatting

--- packages/publish/src/insert.ts
+++ packages/publish/src/insert.ts
@@ -3,29 +3,29 @@ import pg from "pg"
 import getBuild from "./make/getBuild.js"
 import getSourceData from "./make/getSourceData.js"
 import insertEntities from "./make/insertEntities.js"
-    ; (async () => {
-        try {
-            const build = (await getBuild()) + 1
-            console.info("Inserting entities for build:", build)
-            console.info("Loading source data...")
-            const sourceData = await getSourceData({ build })
-            console.info("Loaded source data.")
-            await (async () => {
-                const client = new pg.Client({
-                    database: "phylopic-entities",
-                })
-                try {
-                    await client.connect()
-                    await insertEntities(client, sourceData)
-                } finally {
-                    client.end()
-                }
-            })()
-            console.info("Inserted all entities for build:", build)
-            process.exit(0)
-        } catch (e) {
-            console.info("ERROR!")
-            console.error(e)
-            process.exit(1)
-        }
-    })()
+;(async () => {
+    try {
+        const build = (await getBuild()) + 1
+        console.info("Inserting entities for build:", build)
+        console.info("Loading source data...")
+        const sourceData = await getSourceData({ build })
+        console.info("Loaded source data.")
+        await (async () => {
+            const client = new pg.Client({
+                database: "phylopic-entities",
+            })
+            try {
+                await client.connect()
+                await insertEntities(client, sourceData)
+            } finally {
+                client.end()
+            }
+        })()
+        console.info("Inserted all entities for build:", build)
+        process.exit(0)
+    } catch (e) {
+        console.info("ERROR!")
+        console.error(e)
+        process.exit(1)
+    }
+})()
--- packages/publish/src/release.ts
+++ packages/publish/src/release.ts
@@ -8,31 +8,31 @@ import cleanEntities from "./make/cleanEntities.js"
 import getBuild from "./make/getBuild.js"
 import updateParameters from "./make/updateParameters.js"
 import SOURCE_PATH from "./paths/SOURCE_PATH.js"
-    ; (async () => {
-        const pgClient = new pg.Client({
-            database: "phylopic-entities",
-        })
-        const s3Client = new S3Client({})
-        try {
-            const [, prevBuild, source] = await Promise.all([
-                pgClient.connect(),
-                getBuild(),
-                readJSON<Source>(SOURCE_PATH, isSource),
-            ])
-            const build = prevBuild + 1
-            console.info("Releasing build", build, "...")
-            await updateParameters(build, source.root)
-            console.info("Build", build, "released.")
-            console.info("Cleaning up entities database...")
-            await cleanEntities(pgClient, build)
-            console.info("Cleaned up entities database.")
-        } catch (e) {
-            console.info("ERROR!")
-            console.error(e)
-            process.exit(1)
-        } finally {
-            s3Client.destroy()
-            pgClient.end()
-        }
-        process.exit(0)
-    })()
+;(async () => {
+    const pgClient = new pg.Client({
+        database: "phylopic-entities",
+    })
+    const s3Client = new S3Client({})
+    try {
+        const [, prevBuild, source] = await Promise.all([
+            pgClient.connect(),
+            getBuild(),
+            readJSON<Source>(SOURCE_PATH, isSource),
+        ])
+        const build = prevBuild + 1
+        console.info("Releasing build", build, "...")
+        await updateParameters(build, source.root)
+        console.info("Build", build, "released.")
+        console.info("Cleaning up entities database...")
+        await cleanEntities(pgClient, build)
+        console.info("Cleaned up entities database.")
+    } catch (e) {
+        console.info("ERROR!")
+        console.error(e)
+        process.exit(1)
+    } finally {
+        s3Client.destroy()
+        pgClient.end()
+    }
+    process.exit(0)
+})()
