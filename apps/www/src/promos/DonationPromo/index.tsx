8bfbdde58e299a6d2b0fc75553d2442b98e3e825
Mike Keesey <keesey@gmail.com>
2022-06-01 09:46:15 -0700

    Added donation promo variants

--- apps/www/src/promos/DonationPromo/index.tsx
+++ apps/www/src/promos/DonationPromo/index.tsx
@@ -1,14 +1,21 @@
-import { FC } from "react"
+import clsx from "clsx"
+import { FC, useEffect, useState } from "react"
 import AnchorLink from "~/ui/AnchorLink"
 import SiteTitle from "~/ui/SiteTitle"
 import styles from "./index.module.scss"
 const DonationPromo: FC = () => {
+    const [variant, setVariant] = useState("variantA")
+    useEffect(() => {
+        const index = Math.floor(Math.random() * 3)
+        setVariant(["variantA", "variantB", "variantC"][index])
+    }, [])
     return (
-        <aside className={styles.main}>
+        <aside className={clsx([styles.main, styles[variant]])}>
             <p>
                 Hey, do you like <SiteTitle />? <AnchorLink href="/donate">Make a donation</AnchorLink> to keep it
                 going!
             </p>
+            <p>Or, <a href="https://www.patreon.com/tmkeesey?fan_landing=true">become a patron</a> to see previews of new features!</p>
         </aside>
     )
 }
