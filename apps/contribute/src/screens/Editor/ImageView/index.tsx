1a997a2c9fad4725a31c72a3214ab76467488a7d
Mike Keesey <keesey@gmail.com>
2022-07-25 12:19:20 -0700

    Revisions for ImageView

--- apps/contribute/src/screens/Editor/ImageView/index.tsx
+++ apps/contribute/src/screens/Editor/ImageView/index.tsx
@@ -1,8 +1,9 @@
+import { AnchorLink, Loader } from "@phylopic/ui"
 import { UUID } from "@phylopic/utils"
-import { FC } from "react"
+import { useRouter } from "next/router"
+import { FC, useEffect } from "react"
 import useSWR from "swr"
 import useContributorUUID from "~/auth/hooks/useContributorUUID"
-import useEmailAddress from "~/auth/hooks/useEmailAddress"
 import getSubmissionKey from "~/s3/keys/submissions/getSubmissionKey"
 import fetchExists from "~/swr/fetchExists"
 import FileView from "~/ui/FileView"
@@ -13,8 +14,19 @@ const ImageView: FC<Props> = ({ uuid }) => {
     const contributorUUID = useContributorUUID()
     const sourceKey = `/api/images/${encodeURIComponent(uuid)}/source`
     const submissionKey = contributorUUID ? "/api/" + getSubmissionKey(contributorUUID, uuid) : null
-    const { data: hasSource } = useSWR(sourceKey, fetchExists)
-    const { data: hasSubmission } = useSWR(submissionKey, fetchExists)
+    const { data: hasSource, isValidating: sourceIsValidating } = useSWR(sourceKey, fetchExists)
+    const { data: hasSubmission, isValidating: submissionIsValidating } = useSWR(submissionKey, fetchExists)
+    const isValidating = sourceIsValidating || submissionIsValidating
+    const fileHRef = `/edit/${encodeURIComponent(uuid)}/file`
+    const router = useRouter()
+    useEffect(() => {
+        if (!isValidating && !hasSource && !hasSubmission) {
+            router.push(fileHRef)
+        }
+    }, [fileHRef, hasSource, hasSubmission, isValidating, router])
+    if (!hasSource && !hasSubmission && isValidating) {
+        return <Loader />
+    }
     return (
         <section>
             {hasSource && (
@@ -29,7 +41,11 @@ const ImageView: FC<Props> = ({ uuid }) => {
                     <figcaption>Pending Submission</figcaption>
                 </figure>
             )}
-            {!hasSource && !hasSubmission && "You need to upload an image."}
+            {!hasSource && !hasSubmission && (
+                <>
+                    You need to <AnchorLink className="text" href={fileHRef}> upload an image</AnchorLink>.
+                </>
+            )}
         </section>
     )
 }
