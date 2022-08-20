4259193e6aadc0b4a7a849ee126c5d8ab50366a8
Mike Keesey <keesey@gmail.com>
2022-08-20 13:57:54 -0700

    Fix for Submitted Page

--- apps/contribute/pages/images/submitted.tsx
+++ apps/contribute/pages/images/submitted.tsx
@@ -3,7 +3,6 @@ import type { NextPage } from "next"
 import dynamic from "next/dynamic"
 import AuthorizedOnly from "~/auth/AuthorizedOnly"
 import PageLayout from "~/pages/PageLayout"
-import Dialogue from "~/ui/Dialogue"
 import Speech from "~/ui/Speech"
 const Images = dynamic(() => import("~/screens/Images"), { ssr: false })
 const Page: NextPage = () => (
@@ -17,22 +16,16 @@ const Page: NextPage = () => (
             <Images filter="submitted">
                 {total =>
                     typeof total !== "number" ? (
-                        <Dialogue>
-                            <Speech mode="system">
-                                <p>Loading submissions&hellip;</p>
-                                <Loader />
-                            </Speech>
-                        </Dialogue>
+                        <>
+                            <p>Loading submissions&hellip;</p>
+                            <Loader />
+                        </>
                     ) : (
-                        <Dialogue>
-                            <Speech mode="system">
-                                <p>
-                                    {total === 1 ? "This is the image" : "These are the images"} you have submitted,
-                                    which {total === 1 ? "is" : "are"} pending review. Click on{" "}
-                                    {total === 1 ? "it" : "any of them"} if you want to make any changes first.
-                                </p>
-                            </Speech>
-                        </Dialogue>
+                        <p>
+                            {total === 1 ? "This is the image" : "These are the images"} you have submitted which{" "}
+                            {total === 1 ? "is" : "are"} pending review. Click on {total === 1 ? "it" : "any of them"}{" "}
+                            if you want to make any changes first.
+                        </p>
                     )
                 }
             </Images>
