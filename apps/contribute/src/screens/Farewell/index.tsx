9cba3000ae30958b0f79a040abe06cc3c47e207e
Mike Keesey <keesey@gmail.com>
2022-08-20 14:41:58 -0700

    Added site link to Logout Page

--- apps/contribute/src/screens/Farewell/index.tsx
+++ apps/contribute/src/screens/Farewell/index.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react"
 import Dialogue from "~/ui/Dialogue"
-import { ICON_ARROW_RIGHT } from "~/ui/ICON_SYMBOLS"
+import { ICON_ARROW_LEFT, ICON_ARROW_RIGHT } from "~/ui/ICON_SYMBOLS"
 import Speech from "~/ui/Speech"
 import UserLinkButton from "~/ui/UserLinkButton"
 import UserOptions from "~/ui/UserOptions"
@@ -11,8 +11,11 @@ const Farewell: FC = () => {
                 <p>You have logged out.</p>
             </Speech>
             <UserOptions>
-                <UserLinkButton icon={ICON_ARROW_RIGHT} href="/">
+                <UserLinkButton icon={ICON_ARROW_LEFT} href="/">
                     Log me back in.
+                </UserLinkButton>
+                <UserLinkButton icon={ICON_ARROW_RIGHT} href={`https://${process.env.NEXT_PUBLIC_WWW_DOMAIN}`}>
+                    View the site.
                 </UserLinkButton>
             </UserOptions>
         </Dialogue>
