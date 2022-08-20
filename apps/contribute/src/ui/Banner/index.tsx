cfb0b195d635e4143a523bd872b060f062b58a8e
Mike Keesey <keesey@gmail.com>
2022-08-19 17:33:35 -0700

    Cleanup for contribute UI

--- apps/contribute/src/ui/Banner/index.tsx
+++
@@ -1,9 +0,0 @@
-import { FC, ReactNode } from "react"
-import styles from "./index.module.scss"
-export type Props = {
-    children?: ReactNode
-}
-const Banner: FC<Props> = ({ children }) => {
-    return <section className={styles.main}>{children}</section>
-}
-export default Banner
