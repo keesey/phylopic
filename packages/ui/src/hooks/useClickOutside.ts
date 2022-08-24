07313c4eb70b892bfd231738c0b1a0ba078362a2
Mike Keesey <keesey@gmail.com>
2022-08-24 09:25:24 -0700

    Revised useClickOutside

--- packages/ui/src/hooks/useClickOutside.ts
+++ packages/ui/src/hooks/useClickOutside.ts
@@ -1,16 +1,18 @@
 import { useEffect } from "react"
-export const useClickOutside = (element: Element | null, callback: () => void) => {
+export const useClickOutside = (element: Element | null | undefined, callback?: () => void) => {
     useEffect(() => {
-        const handler = (event: Event) => {
-            if (element && !element.contains(event.target as Node | null)) {
-                callback()
+        if (callback && element) {
+            const handler = (event: Event) => {
+                if (!element.contains(event.target as Node | null)) {
+                    callback()
+                }
             }
-        }
-        document.addEventListener("mousedown", handler)
-        document.addEventListener("touchstart", handler)
-        return () => {
-            document.removeEventListener("mousedown", handler)
-            document.removeEventListener("touchstart", handler)
+            document.addEventListener("mousedown", handler)
+            document.addEventListener("touchstart", handler)
+            return () => {
+                document.removeEventListener("mousedown", handler)
+                document.removeEventListener("touchstart", handler)
+            }
         }
     }, [callback, element])
 }
