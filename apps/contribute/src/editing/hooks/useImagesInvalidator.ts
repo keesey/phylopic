b98c836c108ed42facfaf38e0e1f0895376c92ce
Mike Keesey <keesey@gmail.com>
2022-08-24 17:37:22 -0700

    Fix for images invalidation

--- apps/contribute/src/editing/hooks/useImagesInvalidator.ts
+++ apps/contribute/src/editing/hooks/useImagesInvalidator.ts
@@ -7,11 +7,11 @@ const useImagesInvalidator = () => {
     const { mutate: mutateSubmitted } = useSWR("/api/images?filter=submitted&page=0")
     const { mutate: mutateWithdrawn } = useSWR("/api/images?filter=withdrawn&page=0")
     return useCallback(() => {
-        mutateCount(undefined, { revalidate: true })
-        mutateAccepted(undefined, { revalidate: true })
-        mutateIncomplete(undefined, { revalidate: true })
-        mutateSubmitted(undefined, { revalidate: true })
-        mutateWithdrawn(undefined, { revalidate: true })
+        mutateCount(async (x: any) => x, { revalidate: true })
+        mutateAccepted(async (x: any) => x, { revalidate: true })
+        mutateIncomplete(async (x: any) => x, { revalidate: true })
+        mutateSubmitted(async (x: any) => x, { revalidate: true })
+        mutateWithdrawn(async (x: any) => x, { revalidate: true })
     }, [mutateAccepted, mutateCount, mutateIncomplete, mutateSubmitted, mutateWithdrawn])
 }
 export default useImagesInvalidator
--- apps/contribute/src/ui/UserImageThumbnail/index.tsx
+++ apps/contribute/src/ui/UserImageThumbnail/index.tsx
@@ -3,6 +3,7 @@ import { stringifyNomen, UUID } from "@phylopic/utils"
 import { FC } from "react"
 import useImageNode from "~/editing/hooks/useImageNode"
 import useImageSrc from "~/editing/hooks/useImageSrc"
+import FileThumbnailView from "../FileThumbnailView"
 import NameView from "../NameView"
 import styles from "./index.module.scss"
 export type Props = {
@@ -11,14 +12,10 @@ export type Props = {
 const UserImageThumbnail: FC<Props> = ({ uuid }) => {
     const src = useImageSrc(uuid)
     const specific = useImageNode(uuid, "specific")
-    if (!src) {
-        return null
-    }
     return (
         <figure>
-            <img
-                alt={specific ? stringifyNomen(specific.names[0]) : "silhouette image"}
-                className={styles.main}
+            <FileThumbnailView
+                alt={specific ? stringifyNomen(specific.names[0]) : undefined}
                 src={src}
             />
             <figcaption className={styles.caption}>
