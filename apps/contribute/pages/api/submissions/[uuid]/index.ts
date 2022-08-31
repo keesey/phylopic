d871a84b115cfee631279e1496c710c4ec6a8c1a
Mike Keesey <keesey@gmail.com>
2022-08-31 08:29:37 -0700

    Revert "Revised contribute api methods (POST spawn, PUT submission/[uuid]"
    This reverts commit 795da622c1e02cd6af8eb56cc6f29858935e085c.

--- apps/contribute/pages/api/submissions/[uuid]/index.ts
+++ apps/contribute/pages/api/submissions/[uuid]/index.ts
@@ -1,6 +1,6 @@
 import { handleAPIError } from "@phylopic/source-client"
 import { isSubmission, Submission } from "@phylopic/source-models"
-import { isUUIDv4, stringifyNormalized, UUID, ValidationError, ValidationFaultCollector } from "@phylopic/utils"
+import { isUUIDv4, UUID, ValidationError, ValidationFaultCollector } from "@phylopic/utils"
 import { NextApiHandler } from "next"
 import verifyAuthorization from "~/auth/http/verifyAuthorization"
 import SourceClient from "~/source/SourceClient"
@@ -14,75 +14,51 @@ const index: NextApiHandler<Submission & { uuid: UUID }> = async (req, res) => {
         client = new SourceClient()
         const submissionClient = client.submission(uuid)
         if (!(await submissionClient.exists())) {
-            if (req.method === "PUT") {
-                const { sub: contributor } = (await verifyAuthorization(req.headers)) ?? {}
-                if (!isUUIDv4(contributor)) {
-                    throw 401
+            throw 404
+        }
+        const submission = await submissionClient.get()
+        await verifyAuthorization(req.headers, { sub: submission.contributor })
+        switch (req.method) {
+            case "DELETE": {
+                await submissionClient.delete()
+                res.status(204)
+                break
+            }
+            case "GET":
+            case "HEAD": {
+                res.status(200)
+                res.json(submission)
+                break
+            }
+            case "OPTIONS": {
+                res.setHeader("allow", "DELETE, GET, HEAD, OPTIONS, PATCH")
+                res.status(204)
+                break
+            }
+            case "PATCH": {
+                const newValue: Submission & { uuid: UUID } = {
+                    ...submission,
+                    ...req.body,
+                    uuid,
+                }
+                if (
+                    newValue.contributor !== submission.contributor ||
+                    newValue.created !== submission.created ||
+                    newValue.uuid !== submission.uuid ||
+                    Object.keys(newValue).length !== Object.keys(submission).length
+                ) {
+                    throw 400
                 }
                 const faultCollector = new ValidationFaultCollector(["body"])
-                if (!isSubmission(req.body, faultCollector)) {
+                if (!isSubmission(newValue, faultCollector)) {
                     throw new ValidationError(faultCollector.list(), "Invalid payload.")
                 }
-                await submissionClient.put({
-                    ...req.body,
-                    contributor,
-                    created: new Date().toISOString(),
-                    uuid,
-                })
+                submissionClient.put(newValue)
                 res.status(204)
-            } else {
-                throw 404
+                break
             }
-        } else {
-            const submission = await submissionClient.get()
-            await verifyAuthorization(req.headers, { sub: submission.contributor })
-            switch (req.method) {
-                case "DELETE": {
-                    await submissionClient.delete()
-                    res.status(204)
-                    break
-                }
-                case "GET":
-                case "HEAD": {
-                    res.status(200)
-                    res.json(submission)
-                    break
-                }
-                case "OPTIONS": {
-                    res.setHeader("allow", "DELETE, GET, HEAD, OPTIONS, PATCH, PUT")
-                    res.status(204)
-                    break
-                }
-                case "PATCH":
-                case "PUT": {
-                    const newValue: Submission & { uuid: UUID } = {
-                        ...submission,
-                        ...req.body,
-                        uuid,
-                    }
-                    if (
-                        newValue.contributor !== submission.contributor ||
-                        newValue.created !== submission.created ||
-                        newValue.uuid !== submission.uuid ||
-                        Object.keys(newValue).length !== Object.keys(submission).length
-                    ) {
-                        throw 400
-                    }
-                    if (stringifyNormalized(submission) === stringifyNormalized(newValue)) {
-                        res.status(304)
-                    } else {
-                        const faultCollector = new ValidationFaultCollector(["body"])
-                        if (!isSubmission(newValue, faultCollector)) {
-                            throw new ValidationError(faultCollector.list(), "Invalid payload.")
-                        }
-                        await submissionClient.put(newValue)
-                        res.status(204)
-                    }
-                    break
-                }
-                default: {
-                    throw 405
-                }
+            default: {
+                throw 405
             }
         }
     } catch (e) {
