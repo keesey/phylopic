8de7f66df32fda02ca8e058a226e77c6b9e7a752
Mike Keesey <keesey@gmail.com>
2022-09-02 21:49:08 -0700

    Revised SiteMenu

--- apps/contribute/src/ui/SiteNav/DropDownMenu/menus/SiteMenu/index.tsx
+++ apps/contribute/src/ui/SiteNav/DropDownMenu/menus/SiteMenu/index.tsx
@@ -1,24 +1,19 @@
 import { FC } from "react"
 import { ICON_HAND_POINT_RIGHT } from "~/ui/ICON_SYMBOLS"
-import SiteTitle from "~/ui/SiteTitle"
 import MenuLink from "../../MenuLink"
 const SiteMenu: FC = () => {
     return (
         <>
-            <li>
-                <h2>
-                    <SiteTitle />: Contribute
-                </h2>
-            </li>
+            <MenuLink
+                href="/"
+                icon={ICON_HAND_POINT_RIGHT}
+                label="Contribute"
+            />
             <MenuLink
                 href={`https://${process.env.NEXT_PUBLIC_WWW_DOMAIN}`}
                 icon={ICON_HAND_POINT_RIGHT}
                 isExternal
-                label={
-                    <>
-                        <SiteTitle /> (Main Site)
-                    </>
-                }
+                label="Main Site"
             />
             <MenuLink
                 href={`https://${process.env.NEXT_PUBLIC_WWW_DOMAIN}/images`}
