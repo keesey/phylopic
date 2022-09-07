285f7631c0211d306af05f763fcc157b1a68bd2e
Mike Keesey <keesey@gmail.com>
2022-09-07 08:41:00 -0700

    Added cache-busting for build checker

--- apps/www/src/pages/PageLayout/BuildChecker.tsx
+++ apps/www/src/pages/PageLayout/BuildChecker.tsx
@@ -1,9 +1,12 @@
+import { createSearch } from "@phylopic/utils"
 import { useAPIFetcher } from "@phylopic/utils-api"
-import { FC } from "react"
+import { FC, useEffect, useState } from "react"
 import useSWRImmutable from "swr/immutable"
 const BuildChecker: FC = () => {
     const fetcher = useAPIFetcher()
-    useSWRImmutable(`https://${process.env.NEXT_PUBLIC_API_DOMAIN}/`, fetcher)
+    const [cachebuster, setCachebuster] = useState<string | undefined>()
+    useEffect(() => setCachebuster(new Date().valueOf().toString(16)), [])
+    useSWRImmutable(`https://${process.env.NEXT_PUBLIC_API_DOMAIN}/${createSearch({ cb: cachebuster })}`, fetcher)
     return null
 }
 export default BuildChecker
