0378636ca71594dc2c2fe3ddbc027fc22a478d51
Mike Keesey <keesey@gmail.com>
2022-09-13 08:18:23 -0700

    Formatted revalidate.ts

--- apps/publish/src/revalidate.ts
+++ apps/publish/src/revalidate.ts
@@ -1,9 +1,13 @@
-import axios from "axios";
-import "dotenv/config";
-(async () => {
+import axios from "axios"
+import "dotenv/config"
+;(async () => {
     try {
         console.info("Revalidating website...")
-        const response = await axios.get<{ revalidated: boolean }>(`https://${process.env.WWW_DOMAIN}/api/revalidate?secret=${encodeURIComponent(process.env.REVALIDATE_KEY ?? "")}`)
+        const response = await axios.get<{ revalidated: boolean }>(
+            `https://${process.env.WWW_DOMAIN}/api/revalidate?secret=${encodeURIComponent(
+                process.env.REVALIDATE_KEY ?? "",
+            )}`,
+        )
         if (!response.data.revalidated) {
             throw new Error("Failed to revalidate.")
         }
