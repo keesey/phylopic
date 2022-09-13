7088cbbff3ee4e3d93e93279d2ff392ae5dfcb4e
Mike Keesey <keesey@gmail.com>
2022-09-13 06:50:14 -0700

    Formatted sendAuthEmail.ts

--- apps/contribute/src/auth/smtp/sendAuthEmail.ts
+++ apps/contribute/src/auth/smtp/sendAuthEmail.ts
@@ -24,9 +24,9 @@ const sendAuthEmail = async (email: EmailAddress, token: JWT, now: Date, expires
     if (duration < 0) {
         throw new Error("Tried to send an email using an expired token.")
     }
-    const url = `https://${
-        process.env.NEXT_PUBLIC_CONTRIBUTE_DOMAIN
-    }/authorize/${encodeURIComponent(email)}/${encodeURIComponent(payload.jti)}`
+    const url = `https://${process.env.NEXT_PUBLIC_CONTRIBUTE_DOMAIN}/authorize/${encodeURIComponent(
+        email,
+    )}/${encodeURIComponent(payload.jti)}`
     const client = new SESClient({
         credentials: {
             accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
@@ -35,36 +35,38 @@ const sendAuthEmail = async (email: EmailAddress, token: JWT, now: Date, expires
         region: process.env.AWS_REGION!,
     })
     try {
-        const response = await client.send(new SendEmailCommand({
-            Message: {
-                Body: {
-                    Html: {
-                        Data: `<p>Open this link to start uploading images to <i>PhyloPic</i>: <a href="${url}"><code>${url}</code></a>.
+        const response = await client.send(
+            new SendEmailCommand({
+                Message: {
+                    Body: {
+                        Html: {
+                            Data: `<p>Open this link to start uploading images to <i>PhyloPic</i>: <a href="${url}"><code>${url}</code></a>.
 This link will expire at ${expires.toUTCString()}.</p>
 <p>Thanks! Can't wait to see your silhouettes.</p>
 <br />
 Mike Keesey<br />
-<a href="mailto:keesey+phylopic@gmail.com"><code>keesey+phylopic@gmail.com</code></a>`
-                    },
-                    Text: {
-                        Data: `Open this link to start uploading images to PhyloPic: ${url}
+<a href="mailto:keesey+phylopic@gmail.com"><code>keesey+phylopic@gmail.com</code></a>`,
+                        },
+                        Text: {
+                            Data: `Open this link to start uploading images to PhyloPic: ${url}
 This link will expire at ${expires.toUTCString()}.
 
 Thanks! Can't wait to see your silhouettes.
 
 Mike Keesey
-keesey+phylopic@gmail.com`
-                    }
+keesey+phylopic@gmail.com`,
+                        },
+                    },
+                    Subject: {
+                        Data: "Uploading Silhouettes to PhyloPic",
+                    },
                 },
-                Subject: {
-                    Data: "Uploading Silhouettes to PhyloPic"
-                }
-            },
-            Destination: {
-                ToAddresses: [email],
-            },
-            Source: `PhyloPic Contributions <no-reply@mail.phylopic.org>`
-        }))
+                Destination: {
+                    ToAddresses: [email],
+                },
+                Source: `PhyloPic Contributions <no-reply@mail.phylopic.org>`,
+            }),
+        )
         console.info("[EMAIL SENT]", response)
     } finally {
         client.destroy()
