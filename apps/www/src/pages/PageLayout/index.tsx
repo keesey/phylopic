7dc5b076816da435f216ad9697c5ac1c0bde47ea
Mike Keesey <keesey@gmail.com>
2022-09-14 14:14:38 -0700

    Fixed www layout

--- apps/www/src/pages/PageLayout/index.tsx
+++ apps/www/src/pages/PageLayout/index.tsx
@@ -25,26 +25,23 @@ const PageLayout: FC<Props> = ({ aside, build, children, fallback = {}, initialT
         }
     }, [])
     return (
-        <>
-            <Analytics />
-            <SWRConfig value={{ fallback }}>
-                <BuildContainer initialValue={build}>
-                    <BuildChecker />
-                    <PageLoader />
-                    {aside && <aside key="aside">{aside}</aside>}
-                    <SearchContainer initialText={initialText}>
-                        <Search />
-                        <header>
-                            <SiteNav />
-                        </header>
-                        <main>
-                            <SearchOverlay>{children}</SearchOverlay>
-                        </main>
-                        <SiteFooter />
-                    </SearchContainer>
-                </BuildContainer>
-            </SWRConfig>
-        </>
+        <SWRConfig value={{ fallback }}>
+            <BuildContainer initialValue={build}>
+                <BuildChecker />
+                <PageLoader />
+                {aside && <aside key="aside">{aside}</aside>}
+                <SearchContainer initialText={initialText}>
+                    <Search />
+                    <header>
+                        <SiteNav />
+                    </header>
+                    <main>
+                        <SearchOverlay>{children}</SearchOverlay>
+                    </main>
+                    <SiteFooter />
+                </SearchContainer>
+            </BuildContainer>
+        </SWRConfig>
     )
 }
 export default PageLayout
