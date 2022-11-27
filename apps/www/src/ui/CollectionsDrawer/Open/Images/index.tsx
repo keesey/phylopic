15e05fcd8ce6316fbfaa60b20379a824f1ff6d98
Mike Keesey <keesey@gmail.com>
2022-11-26 22:57:39 -0800

    Tweaks

--- apps/www/src/ui/CollectionsDrawer/Open/Images/index.tsx
+++ apps/www/src/ui/CollectionsDrawer/Open/Images/index.tsx
@@ -2,6 +2,7 @@ import { AnchorLink, ImageThumbnailView } from "@phylopic/ui"
 import { FC, useContext } from "react"
 import CollectionsContext from "~/collections/context/CollectionsContext"
 import useCurrentCollectionImages from "~/collections/hooks/useCurrentCollectionImages"
+import LinkedImageThumbnailView from "~/views/LinkedImageThumbnailView"
 import styles from "./index.module.scss"
 const Images: FC = () => {
     const images = useCurrentCollectionImages()
@@ -10,13 +11,12 @@ const Images: FC = () => {
         <section className={styles.main}>
             {images.map(image => (
                 <section key={image.uuid}>
-                    <AnchorLink href={`/images/${encodeURIComponent(image.uuid)}`}>
-                        <ImageThumbnailView value={image} />
-                    </AnchorLink>
+                    <LinkedImageThumbnailView value={image} />
                     <div>
                         <a
                             className={styles.close}
                             onClick={() => dispatch({ type: "REMOVE_FROM_CURRENT_COLLECTION", payload: image.uuid })}
+                            title="Remove"
                         >
                             ✖
                         </a>
