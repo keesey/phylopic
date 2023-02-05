a6d4fcf9ad5ac91e8ec508a8033b7fd5df967002
Mike Keesey <keesey@gmail.com>
2023-02-05 15:46:55 -0800

    Canonical permalink URL fix

--- apps/contribute/src/screens/Assignment/AssignmentContainer/hooks/usePending.ts
+++ apps/contribute/src/screens/Assignment/AssignmentContainer/hooks/usePending.ts
@@ -3,7 +3,7 @@ import AssignmentContext from "../AssignmentContext"
 const usePending = () => {
     const context = useContext(AssignmentContext)
     if (context && !context.length) {
-        return true;
+        return true
     }
     return context?.[0]?.pending ?? false
 }
