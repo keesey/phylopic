0b983f56b4b3c3d86464de1ccb3e209279d46e23
Mike Keesey <keesey@gmail.com>
2023-02-12 12:21:14 -0800

    Metadata improvements, including rich metadata for API Docs.

---
+++ apps/contribute/pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from "next/document"
+
+export default function Document() {
+    return (
+        <Html lang="en" prefix="og: https://ogp.me/ns#">
+            <Head />
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
