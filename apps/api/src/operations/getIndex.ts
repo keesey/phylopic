9944bd0415e4cbc452b282c08259cf1de61afd72
Mike Keesey <keesey@gmail.com>
2023-02-11 23:35:59 -0800

    Tweaks to root API response

--- apps/api/src/operations/getIndex.ts
+++ apps/api/src/operations/getIndex.ts
@@ -1,6 +1,5 @@
 import { DataParameters, DATA_MEDIA_TYPE } from "@phylopic/api-models"
 import { APIGatewayProxyResult } from "aws-lambda"
-import pkg from "../../package.json"
 import BUILD from "../build/BUILD"
 import checkBuild from "../build/checkBuild"
 import createBuildRedirect from "../build/createBuildRedirect"
@@ -10,9 +9,9 @@ import PERMANENT_HEADERS from "../headers/responses/PERMANENT_HEADERS"
 import checkAccept from "../mediaTypes/checkAccept"
 import { Operation } from "./Operation"
 export type GetRootParameters = DataRequestHeaders & DataParameters
-const BODY = `{"_links":{"contact":{"href":"mailto:keesey+phylopic@gmail.com","title":"Mike Keesey"},"documentation":{"href":"http://api-docs.phylopic.org/2.0"},"resources":[{"href":"/autocomplete?build=${BUILD}","title":"Autocomplete for Phylogenetic Nodes"},{"href":"/collections","title":"Entity Collections"},{"href":"/contributors?build=${BUILD}","title":"Image Contributors"},{"href":"/images?build=${BUILD}","title":"Silhouette Images"},{"href":"/licenses?build=${BUILD}","title":"Licenses for New Submissions"},{"href":"/nodes?build=${BUILD}","title":"Phylogenetic Nodes"},{"href":"/ping","title":"API Check"},{"href":"/root?build=${BUILD}","title":"Phylogenetic Root Node"},{"href":"/uploads","title":"Image Submission Uploads"}],"self":{"href":"/?build=${BUILD}"}},"build":${BUILD},"buildTimestamp":${JSON.stringify(
+const BODY = `{"_links":{"contact":{"href":"mailto:keesey+phylopic@gmail.com","title":"Mike Keesey"},"documentation":{"href":"http://api-docs.phylopic.org/v2"},"resources":[{"href":"/autocomplete?build=${BUILD}","title":"Autocomplete for Phylogenetic Nodes"},{"href":"/collections","title":"Entity Collections"},{"href":"/contributors?build=${BUILD}","title":"Image Contributors"},{"href":"/images?build=${BUILD}","title":"Silhouette Images"},{"href":"/licenses?build=${BUILD}","title":"Licenses for New Submissions"},{"href":"/nodes?build=${BUILD}","title":"Phylogenetic Nodes"},{"href":"/ping","title":"API Check"},{"href":"/root?build=${BUILD}","title":"Phylogenetic Root Node"},{"href":"/uploads","title":"Image Submission Uploads"}],"self":{"href":"/?build=${BUILD}"}},"build":${BUILD},"buildTimestamp":${JSON.stringify(
     process.env.PHYLOPIC_BUILD_TIMESTAMP,
-)},"title":"PhyloPic Application Programming Interface","version":${JSON.stringify(pkg.version)}}`
+)},"title":"PhyloPic Application Programming Interface","version":"2.1.1"}`
 const RESULT_200: APIGatewayProxyResult = {
     body: BODY,
     headers: { ...DATA_HEADERS, ...PERMANENT_HEADERS },
