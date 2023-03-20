76f9c0a411ade805cfc1407725fb9e9084548bf0
Mike Keesey <keesey@gmail.com>
2023-03-20 08:41:46 -0700

    Contribute: redirect for social media image was not implemented correctly.

--- apps/contribute/next.config.js
+++ apps/contribute/next.config.js
@@ -4,12 +4,14 @@ module.exports = {
         domains: ["images.phylopic.org"],
     },
     reactStrictMode: true,
-    redirects: [
-        {
-            source: "/social/1200x1200.png",
-            destination: "https://images.phylopic.org/social/1200x1200.png",
-            permanent: true,
-        },
-    ],
+    async redirects() {
+        return [
+            {
+                source: "/social/1200x1200.png",
+                destination: "https://images.phylopic.org/social/1200x1200.png",
+                permanent: true,
+            },
+        ]
+    },
     swcMinify: true,
 }
