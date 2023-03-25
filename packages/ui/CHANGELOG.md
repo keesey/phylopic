db0bd845d1e29bd793695ed1a122d66aa315ebe4
Mike Keesey <keesey@gmail.com>
2023-03-24 18:32:42 -0700

    Added cache-buster based on modification date for image file views.

--- packages/ui/CHANGELOG.md
+++ packages/ui/CHANGELOG.md
@@ -9,6 +9,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Added
 
+-   Cache-buster based on modification date for image file views.
+
 ### Changed
 
 ### Deprecated
--- packages/ui/src/views/ImageThumbnailView/index.tsx
+++ packages/ui/src/views/ImageThumbnailView/index.tsx
@@ -6,7 +6,7 @@ export interface ImageThumbnailViewProps {
     value: Image
 }
 export const ImageThumbnailView: React.FC<ImageThumbnailViewProps> = ({ value }) => {
-    const loader = useImageLoader(value._links.thumbnailFiles)
+    const loader = useImageLoader(value._links.thumbnailFiles, value.modifiedFile)
     return <NextImage alt={value._links.self.title} height={64} loader={loader} src={value.uuid} width={64} />
 }
 export default ImageThumbnailView
