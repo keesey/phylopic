4fc3757389a8f2c03954347576c20617231fa1c9
Mike Keesey <keesey@gmail.com>
2023-04-03 08:33:11 -0700

    Added UUIDs to Quick Links and made a few tweaks.

--- apps/www/src/ui/QuickLinks/QuickLinkNodeView/index.tsx
+++ apps/www/src/ui/QuickLinks/QuickLinkNodeView/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useMemo } from "react"
+import { FC, Fragment, useMemo } from "react"
 import { QuickLinkNode } from "../QuickLinkNode"
 import styles from "./index.module.scss"
 import getNodeSlug from "~/routes/getNodeSlug"
@@ -23,14 +23,14 @@ const QuickLinkNodeView: FC<Props> = ({ node }) => {
                         {"("}
                     </Link>
                     {children.map((child, index) => (
-                        <>
+                        <Fragment key={child.uuid}>
                             {index > 0 && (
                                 <Link className={styles.operator} href={href} title={title}>
                                     {","}
                                 </Link>
                             )}
                             <QuickLinkNodeView node={child} />
-                        </>
+                        </Fragment>
                     ))}
                     <Link className={styles.operator} href={href} title={title}>
                         {")"}
@@ -46,11 +46,23 @@ const QuickLinkNodeView: FC<Props> = ({ node }) => {
     )
 }
 export default QuickLinkNodeView
-const compareChildren = (a: QuickLinkNode, b: QuickLinkNode) => getLabelCount(a) - getLabelCount(b)
+const compareChildren = (a: QuickLinkNode, b: QuickLinkNode) => {
+    return (
+        getLabelCount(a) - getLabelCount(b) ||
+        getDepth(a) - getDepth(b) ||
+        (a.slug < b.slug ? -1 : a.slug > b.slug ? 1 : 0)
+    )
+}
 const deslugify = (s: string) => capitalize(s.replaceAll("-", " "))
 const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1)
 const getLabelCount = (node: QuickLinkNode): number => {
     const self = node.label ? 1 : 0
     const children = node.children?.reduce<number>((prev, child) => prev + getLabelCount(child), 0) ?? 0
     return self + children
+}
+const getDepth = (node: QuickLinkNode, parentDepth = 0): number => {
+    if (!node.children?.length) {
+        return parentDepth + 1
+    }
+    return node.children.reduce<number>((prev, child) => Math.max(prev, getDepth(child, parentDepth + 1)), 0)
 }
