11772e5cfffc0e9d77fce01f0d41f1b12828b9e3
Mike Keesey <keesey@gmail.com>
2023-04-25 09:04:38 -0700

    Added extra checks to isResolveObjectParameters and isResolveObjectsParameters

--- packages/api-models/src/queryParameters/detection/isResolveObjectParameters.ts
+++ packages/api-models/src/queryParameters/detection/isResolveObjectParameters.ts
@@ -4,10 +4,12 @@ import { NodeEmbedded } from "../../types/NodeWithEmbedded"
 import { NODE_EMBEDDED_PARAMETERS } from "../constants/NODE_EMBEDDED_PARAMETERS"
 import { ResolveObjectParameters } from "../types/ResolveObjectParameters"
 import isEmbeddableParameters from "./isEmbeddableParameters"
+import isDataParameters from "./isDataParameters"
 export const isResolveObjectParameters = (
     x: unknown,
     faultCollector?: ValidationFaultCollector,
 ): x is ResolveObjectParameters =>
+    isDataParameters(x, faultCollector) &&
     isEmbeddableParameters<NodeEmbedded>(NODE_EMBEDDED_PARAMETERS)(x, faultCollector) &&
     isAuthority((x as ResolveObjectParameters).authority, faultCollector?.sub("authority")) &&
     isNamespace((x as ResolveObjectParameters).namespace, faultCollector?.sub("namespace")) &&
--- packages/api-models/src/queryParameters/detection/isResolveObjectsParameters.ts
+++ packages/api-models/src/queryParameters/detection/isResolveObjectsParameters.ts
@@ -4,10 +4,12 @@ import { NodeEmbedded } from "../../types/NodeWithEmbedded"
 import { NODE_EMBEDDED_PARAMETERS } from "../constants/NODE_EMBEDDED_PARAMETERS"
 import { ResolveObjectsParameters } from "../types/ResolveObjectsParameters"
 import isEmbeddableParameters from "./isEmbeddableParameters"
+import isDataParameters from "./isDataParameters"
 export const isResolveObjectsParameters = (
     x: unknown,
     faultCollector?: ValidationFaultCollector,
 ): x is ResolveObjectsParameters =>
+    isDataParameters(x, faultCollector) &&
     isEmbeddableParameters<NodeEmbedded>(NODE_EMBEDDED_PARAMETERS)(x, faultCollector) &&
     isAuthority((x as ResolveObjectsParameters).authority, faultCollector?.sub("authority")) &&
     isNamespace((x as ResolveObjectsParameters).namespace, faultCollector?.sub("namespace")) &&
