97ddab13401259bfb0edeada83eed61d3d745dae
Mike Keesey <keesey@gmail.com>
2023-05-30 08:32:11 -0700

    Fix for www revalidation -- doesn't cover detail pages yet.

--- apps/publish/src/revalidate.ts
+++ apps/publish/src/revalidate.ts
@@ -1,14 +1,21 @@
 import axios from "axios"
 import "dotenv/config"
 ;(async () => {
-    try {
-        console.info("Revalidating website...")
-        const response = await axios.get<{ revalidated: boolean }>(
-            `${process.env.WWW_URL}/api/revalidate?secret=${encodeURIComponent(process.env.REVALIDATE_KEY ?? "")}`,
-        )
-        if (!response.data.revalidated) {
-            throw new Error("Failed to revalidate.")
+    const STATIC_PATHS = ["/", "/contributors", "/images", "/nodes"]
+    const revalidatePath = async (path: string) => {
+        try {
+            const response = await axios.get<{ revalidated: boolean }>(
+                `${process.env.WWW_URL}/api/revalidate?path=${encodeURIComponent(path)}&secret=${encodeURIComponent(process.env.REVALIDATE_TOKEN ?? "")}`,
+            )
+            if (!response.data.revalidated) {
+                console.warn(`Failed to revalidate path: ${path}`)
+            }
+        } catch (e) {
+            console.error(`Error revalidating path: ${path}\n${String(e)}`)
         }
+    }
+    try {
+        await Promise.all(STATIC_PATHS.map(path => revalidatePath(path)))
         console.info("Revalidated website.")
     } catch (e) {
         console.error(e)
