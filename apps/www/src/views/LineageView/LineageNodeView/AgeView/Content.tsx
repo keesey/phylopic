5ec59c38e6febef6e30aca1ed0616ae4f61d233e
Mike Keesey <keesey@gmail.com>
2024-02-17 18:34:53 -0800

    Code refactoring for AgeView

--- apps/www/src/views/LineageView/LineageNodeView/AgeView/Content.tsx
+++ apps/www/src/views/LineageView/LineageNodeView/AgeView/Content.tsx
@@ -6,12 +6,18 @@ import getAgePresentation from "./getAgePresentation"
 const Content: FC<{ ages: AgeResult["ages"]; isTerminal: boolean }> = ({ ages, isTerminal }) => {
     if (isTerminal && ages[0] !== ages[1]) {
         const presentations = ages.map(age => getAgePresentation(age))
+        const sameUnit = presentations[0].years === presentations[1].years
         return (
             <>
                 ~<NumberView value={presentations[0].value} />
-                {presentations[0].years === presentations[1].years ? null : <UnitView {...presentations[0]} />}
-                {presentations[0].years === presentations[1].years ? "–" : " – "}
-                {presentations[0].years === presentations[1].years ? "" : "~"}
+                {sameUnit ? (
+                    "–"
+                ) : (
+                    <>
+                        <UnitView {...presentations[0]} />
+                        {" – ~"}
+                    </>
+                )}
                 <NumberView value={presentations[1].value} />
                 <UnitView {...presentations[1]} />
             </>
