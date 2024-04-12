a7364a12339e16a0a14e2cfe4f5e679788c1ae9b
Mike Keesey <keesey@gmail.com>
2024-04-12 08:48:25 -0700

    UI export issue resolved!

--- packages/ui/src/views/ImageThumbnailView/index.tsx
+++ packages/ui/src/views/ImageThumbnailView/index.tsx
@@ -2,10 +2,15 @@ import { type Image } from "@phylopic/api-models"
 import NextImage from "next/image"
 import React from "react"
 import { useImageLoader } from "../../hooks/useImageLoader"
+// :KLUDGE: Next.js ESM issue.
+let ResolvedImage = NextImage
+if ("default" in ResolvedImage) {
+    ResolvedImage = (ResolvedImage as unknown as { default: typeof NextImage }).default
+}
 export interface ImageThumbnailViewProps {
     value: Image
 }
 export const ImageThumbnailView: React.FC<ImageThumbnailViewProps> = ({ value }) => {
     const loader = useImageLoader(value._links.thumbnailFiles, value.modifiedFile)
-    return <NextImage alt={value._links.self.title} height={64} loader={loader} src={value.uuid} width={64} />
+    return <ResolvedImage alt={value._links.self.title} height={64} loader={loader} src={value.uuid} width={64} />
 }
