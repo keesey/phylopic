87100751cba944ac6e9558350a64c842b3218153
Mike Keesey <keesey@gmail.com>
2024-04-21 18:43:42 -0700

    Minor tweak to formatDate()

--- apps/games/lib/datetime/formatDate.ts
+++ apps/games/lib/datetime/formatDate.ts
@@ -16,4 +16,4 @@ const MONTH_NAMES = [
 ]
 const DAY_OF_WEEK_NAMES = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 export const formatDate = (date: CalendarDate, style?: "long" | "short") =>
-    `${style === "long" ? `${DAY_OF_WEEK_NAMES[toDate(date).getDay()]}, ` : ""}${date.day} ${style === "short" ? MONTH_NAMES[date.month - 1].slice(0, 3) : MONTH_NAMES[date.month - 1]}, ${date.year}`
+    `${style === "long" ? `${DAY_OF_WEEK_NAMES[toDate(date).getDay()]} ` : ""}${date.day} ${style === "short" ? MONTH_NAMES[date.month - 1].slice(0, 3) : MONTH_NAMES[date.month - 1]}, ${date.year}`
