d6480a860fa574b24354940baed686792b627433
Mike Keesey <keesey@gmail.com>
2024-10-20 12:53:21 -0700

    Added new step in the `release` script to invalidate the CloudFront distribution for the API. Upgraded AWS SDKs.

---
+++ apps/publish/src/make/invalidateAPICache.ts
@@ -0,0 +1,26 @@
+import { CloudFrontClient, CreateInvalidationCommand } from "@aws-sdk/client-cloudfront"
+const invalidateAPICache = async () => {
+    const client = new CloudFrontClient()
+    try {
+        if (!process.env.API_CLOUDFRONT_DISTRIBUTION_ID) {
+            console.error("API_CLOUDFRONT_DISTRIBUTION_ID is not set.")
+        }
+        await client.send(
+            new CreateInvalidationCommand({
+                DistributionId: process.env.PHYLOPIC_API_CLOUDFRONT_DISTRIBUTION_ID,
+                InvalidationBatch: {
+                    CallerReference: undefined,
+                    Paths: {
+                        Items: ["/*"],
+                        Quantity: undefined,
+                    },
+                },
+            }),
+        )
+    } catch (e) {
+        console.error(e)
+    } finally {
+        client.destroy()
+    }
+}
+export default invalidateAPICache
