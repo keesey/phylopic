3db8ed350217488208fbd9e445a52d995026a8af
Mike Keesey <keesey@gmail.com>
2025-01-24 13:07:41 -0800

    Better fix for prepareValue()

--- packages/source-client/src/implementations/pg/preparation/prepareValue.ts
+++ packages/source-client/src/implementations/pg/preparation/prepareValue.ts
@@ -4,7 +4,7 @@ export const prepareValue = (x: unknown, type: string): unknown => {
     if (typeof x === "boolean") {
         return prepareBoolean(x)
     } else if (Array.isArray(x) && type.endsWith("[]")) {
-        return `{${x.map(value => prepareValue(value, type)).join(",")}}`
+        return `{${x.map(value => prepareValue(value, type.replace(/\[\]$/, ""))).join(",")}}`
     } else if (x && typeof x === "object") {
         return prepareJSON(x)
     }
