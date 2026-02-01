908829e5327c034ac2dbee82f7fb40ebf167c2f8
Mike Keesey <keesey@gmail.com>
2026-02-01 08:44:14 -0800

    Catching errors for GBIF autolinker so that entire process is not stopped for one failed request.

--- apps/publish/CHANGELOG.md
+++ apps/publish/CHANGELOG.md
@@ -19,6 +19,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Security
 
+## [1.9.2] - 2026-02-01
+
+### Fixed
+
+- Catching errors for GBIF autolinker so that entire process is not stopped for one failed request.
+
 ## [1.9.1] - 2026-01-05
 
 ### Changed
--- apps/publish/src/autolink/gbif.ts
+++ apps/publish/src/autolink/gbif.ts
@@ -42,61 +42,65 @@ const getScientificNames = (names: readonly Nomen[]) =>
 const EXCLUDED_IDS: readonly number[] = [0, 2, 4, 7]
 const sanitizeID = (x: number | undefined) => (typeof x === "number" && !EXCLUDED_IDS.includes(x) ? x : undefined)
 const processNode = async (client: SourceClient, node: Node & { uuid: UUID }, ancestry: Ancestry) => {
-    let mainUsage: GBIFNameUsage | undefined
-    const page = await client.node(node.uuid).externals.namespace("gbif.org", "species").page()
-    if (page.items.length) {
-        mainUsage = (
-            await axios.get<GBIFNameUsage>(
-                `https://api.gbif.org/v1/species/${encodeURIComponent(page.items[0].objectID)}`,
-            )
-        ).data
-    } else {
-        const names = getScientificNames(node.names)
-        for (const name of names) {
-            const search = {
-                ...ancestry,
-                name,
-                strict: true,
-            }
-            const response = await axios.get<GBIFMatch>(`https://api.gbif.org/v1/species/match${createSearch(search)}`)
-            if (response.data.matchType === "EXACT" && typeof response.data.usageKey === "number") {
-                if (!mainUsage) {
-                    mainUsage = response.data
+    try {
+        let mainUsage: GBIFNameUsage | undefined
+        const page = await client.node(node.uuid).externals.namespace("gbif.org", "species").page()
+        if (page.items.length) {
+            mainUsage = (
+                await axios.get<GBIFNameUsage>(
+                    `https://api.gbif.org/v1/species/${encodeURIComponent(page.items[0].objectID)}`,
+                )
+            ).data
+        } else {
+            const names = getScientificNames(node.names)
+            for (const name of names) {
+                const search = {
+                    ...ancestry,
+                    name,
+                    strict: true,
                 }
-                const id = String(response.data.usageKey)
-                const externalClient = client.external("gbif.org", "species", id)
-                if (await externalClient.exists()) {
-                    console.info(`${getIdentifier("gbif.org", "species", id)} is already assigned.`)
-                } else {
-                    console.info(
-                        `${getIdentifier("gbif.org", "species", id)} (${
-                            response.data.canonicalName
-                        }) => phylopic.org/nodes/${node.uuid} (${stringifyNomen(node.names[0])})`,
-                    )
-                    await externalClient.put({
-                        authority: "gbif.org",
-                        namespace: "species",
-                        node: node.uuid,
-                        objectID: id,
-                        title: response.data.canonicalName ?? response.data.scientificName ?? "",
-                    })
+                const response = await axios.get<GBIFMatch>(`https://api.gbif.org/v1/species/match${createSearch(search)}`)
+                if (response.data.matchType === "EXACT" && typeof response.data.usageKey === "number") {
+                    if (!mainUsage) {
+                        mainUsage = response.data
+                    }
+                    const id = String(response.data.usageKey)
+                    const externalClient = client.external("gbif.org", "species", id)
+                    if (await externalClient.exists()) {
+                        console.info(`${getIdentifier("gbif.org", "species", id)} is already assigned.`)
+                    } else {
+                        console.info(
+                            `${getIdentifier("gbif.org", "species", id)} (${
+                                response.data.canonicalName
+                            }) => phylopic.org/nodes/${node.uuid} (${stringifyNomen(node.names[0])})`,
+                        )
+                        await externalClient.put({
+                            authority: "gbif.org",
+                            namespace: "species",
+                            node: node.uuid,
+                            objectID: id,
+                            title: response.data.canonicalName ?? response.data.scientificName ?? "",
+                        })
+                    }
                 }
             }
         }
-    }
-    if (mainUsage) {
-        ancestry = {
-            kingdom: sanitizeID(mainUsage.kingdomKey),
-            phylum: sanitizeID(mainUsage.phylumKey),
-            class: sanitizeID(mainUsage.classKey),
-            order: sanitizeID(mainUsage.orderKey),
-            family: sanitizeID(mainUsage.familyKey),
-            genus: sanitizeID(mainUsage.genusKey),
-            species: sanitizeID(mainUsage.speciesKey),
+        if (mainUsage) {
+            ancestry = {
+                kingdom: sanitizeID(mainUsage.kingdomKey),
+                phylum: sanitizeID(mainUsage.phylumKey),
+                class: sanitizeID(mainUsage.classKey),
+                order: sanitizeID(mainUsage.orderKey),
+                family: sanitizeID(mainUsage.familyKey),
+                genus: sanitizeID(mainUsage.genusKey),
+                species: sanitizeID(mainUsage.speciesKey),
+            }
         }
-    }
-    for await (const child of iterateList(client.node(node.uuid).children)) {
-        await processNode(client, child, ancestry)
+        for await (const child of iterateList(client.node(node.uuid).children)) {
+            await processNode(client, child, ancestry)
+        }
+    } catch (e) {
+        console.error(`Error processing node ${node.uuid}:`, e)
     }
 }
 const autolinkGBIF = async (client: SourceClient): Promise<void> => {
